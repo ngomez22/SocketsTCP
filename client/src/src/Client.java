57f3aafba8e87f2623f03b8e37b7e53b4a68dadd
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-22 11:15:53 -0500

    Add downloads folder to client

--- client/src/src/Client.java
+++ client/src/src/Client.java
@@ -5,17 +5,61 @@ import java.net.*;
 
 
 public class Client {
-	public static void main(String argv[]) throws Exception {
-		String sentence;
-		String modifiedSentence;
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("localhost", 4321);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
-		clientSocket.close();
+	
+	public static final int PORT = 4321;
+	public static final String HOST = "127.0.0.1";
+	
+	private static Socket socket;
+	
+	public static void main(String args[]) throws Exception {
+
+        // Initialize socket
+		String status = startConnection();
+		System.out.println(status);
+		
+		if(!status.startsWith("SUCCESS")) {
+			System.out.println("Bye");
+		}
+		else {
+			
+		}
+		
+        File file = new File("./files/"); //TODO cómo sé qué nombre ponerle?
+        
+        // Get the size of the file
+        long length = file.length();
+        
+        byte[] bytes = new byte[16 * 1024]; //TODO tamaño de los paquetes
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+	}
+	
+	public static String startConnection() {
+		try {
+			socket = new Socket(HOST, PORT);
+			return "SUCCESSFULLY CONNECTED TO " + HOST + ":" + PORT + ".\n";
+		} catch (IllegalArgumentException | NullPointerException | UnknownHostException e) {
+			return "INVALID PARAMETERS. ABORTING CONNECTION";
+		} catch (IOException e) {
+			return "COULD NOT ESTABLISH CONNECITON:\n" + e.getMessage();
+		}
+	}
+	
+	public static boolean getFile(String fname) throws FileNotFoundException {
+		try {
+			FileOutputStream fos = new FileOutputStream(new File("/downloads/testfile.jpg"));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
