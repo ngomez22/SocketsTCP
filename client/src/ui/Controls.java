3b00a9074b21971237ad6f9135dc19542d9d21bf
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-25 14:20:16 -0500

    Add exit prompt when connection is active

--- client/src/ui/Controls.java
+++ client/src/ui/Controls.java
@@ -8,40 +8,38 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
 public class Controls extends JPanel implements ActionListener {
-	
+
 	public static final String DOWNLOAD = "DOWNLOAD";
 	public static final String STOP = "STOP";
-	public static final String CONTINUE = "CONTINUE";
-	
+
 	private GUI gui;
 	private JButton downloadBtn;
 	private JButton stopBtn;
-	private JButton playBtn;
-	
+
 	public Controls(GUI gui) {
 		this.gui = gui;
-		setLayout( new GridLayout(1,3) );
-	    setBorder( new TitledBorder( "Controls" ) );
-	    
-	    downloadBtn = new JButton("Download");
-	    downloadBtn.addActionListener(this);
-	    downloadBtn.setActionCommand(DOWNLOAD);
-	    stopBtn = new JButton("Stop");
-	    stopBtn.addActionListener(this);
-	    stopBtn.setActionCommand(STOP);
-	    playBtn = new JButton("Continue");
-	    playBtn.addActionListener(this);
-	    playBtn.setActionCommand(CONTINUE);
-	    
-	    add(downloadBtn);
-	    add(stopBtn);
-	    add(playBtn);
+		setLayout(new GridLayout(1, 2));
+		setBorder(new TitledBorder("Controls"));
+
+		downloadBtn = new JButton("Download");
+		downloadBtn.addActionListener(this);
+		downloadBtn.setActionCommand(DOWNLOAD);
+		stopBtn = new JButton("Stop");
+		stopBtn.addActionListener(this);
+		stopBtn.setActionCommand(STOP);
+
+		add(downloadBtn);
+		add(stopBtn);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		if (arg0.getActionCommand().equals(DOWNLOAD)) {
+			gui.download("test");
+		}
+		if (arg0.getActionCommand().equals(STOP)) {
+
+		}
 	}
 
 }
--- client/src/ui/GUI.java
+++ client/src/ui/GUI.java
@@ -1,78 +1,104 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 
 import src.Client;
 
 public class GUI extends JFrame {
-	
+
 	private Client client;
 	private Connection connection;
 	private Files files;
 	private Controls controls;
 
-	public GUI () {
+	public GUI() {
 		connection = new Connection(this);
 		files = new Files(this);
 		files.setVisible(false);
 		controls = new Controls(this);
 		controls.setVisible(false);
-		
-		setSize( 530, 530 );
-        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        setTitle( "TCP Client" );
-        setLocationRelativeTo( null );
-        setLayout(new BorderLayout());
-        
-        add(connection, BorderLayout.NORTH);
-        add(files, BorderLayout.CENTER);
-        add(controls, BorderLayout.SOUTH);
+
+		setSize(530, 530);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setTitle("TCP Client");
+		setLocationRelativeTo(null);
+		setLayout(new BorderLayout());
+
+		add(connection, BorderLayout.NORTH);
+		add(files, BorderLayout.CENTER);
+		add(controls, BorderLayout.SOUTH);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if (client != null) {
+					int confirmed = JOptionPane.showConfirmDialog(null, "Are you sure you want to end the connection?", "TCP Client", JOptionPane.YES_NO_OPTION);
+					if (confirmed == JOptionPane.YES_OPTION) {
+						client.endConnection();
+						dispose();
+					}
+				} else {
+					dispose();
+				}
+			}
+		});
 	}
-	
+
 	public void showPanels() {
 		files.setVisible(true);
 		controls.setVisible(true);
 	}
-	
+
 	public void hidePanels() {
 		files.setVisible(false);
 		controls.setVisible(false);
 	}
-	
+
 	public void connect() {
 		client = new Client();
 		String status = client.startConnection();
 		System.out.println(status);
 		connection.changeStatus(status);
-		if(status.startsWith("SUCCESS")) {
+		if (status.startsWith("SUCCESS")) {
 			getDownloads();
 			getDownloadables();
 			showPanels();
 			connection.disconnectButton();
 		}
 	}
-	
+
+	public void download(String fname) {
+		try {
+			System.out.println("hola");
+			client.getFile(fname);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, "No such file exists", "Download", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
 	public void disconnect() {
 		String status = client.endConnection();
+		client = null;
 		System.out.println(status);
 		connection.changeStatus(status);
 		connection.connectButton();
 		hidePanels();
 	}
-	
+
 	public void getDownloadables() {
 		String[] downloadables = client.getFiles();
 		files.updateDownloadables(downloadables);
 	}
-	
+
 	public void getDownloads() {
 		String[] downloads = client.getDownloads();
 		files.updateDownloads(downloads);
 	}
-	
+
 	public static void main(String[] args) {
 		GUI i = new GUI();
 		i.setVisible(true);
